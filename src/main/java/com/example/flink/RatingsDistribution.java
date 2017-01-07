975d5444bd9fefd4fcb94d9343f1aacd1e0bb76e
Ivan Mushketyk <ivan.mushketik@gmail.com>
2017-01-07 15:22:21 +0000

    Add RatingsDistribution example

---
+++ src/main/java/com/example/flink/RatingsDistribution.java
@@ -0,0 +1,58 @@
+package com.example.flink;
+
+import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.types.IntValue;
+import org.apache.flink.util.Collector;
+
+// Get distribution of ratings from MovieLens 100K Dataset:
+// http://grouplens.org/datasets/movielens/
+public class RatingsDistribution {
+
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSource<String> file = env.readTextFile("/Users/ivanmushketyk/SparkCourse/ml-100k/u.data");
+        file.map(new ExtractRating())
+            .groupBy(0)
+            // .reduceGroup(new SumRatingCount())
+            .sum(1)
+            .print();
+    }
+
+    private static class ExtractRating implements MapFunction<String, Tuple2<IntValue, Integer>> {
+        IntValue ratingValue = new IntValue();
+
+        // Reuse rating value and result tuple
+        Tuple2<IntValue, Integer> result = new Tuple2<>(ratingValue, 1);
+
+        @Override
+        public Tuple2<IntValue, Integer> map(String s) throws Exception {
+            // Every line contains tab separated values
+            // user id | item id | rating | timestamp
+            String[] split = s.split("\t");
+
+            int rating = Integer.parseInt(split[2]);
+            ratingValue.setValue(rating);
+
+            return result;
+        }
+    }
+
+    private static class SumRatingCount implements GroupReduceFunction<Tuple2<IntValue, Integer>, Tuple2<IntValue, Integer>> {
+        @Override
+        public void reduce(Iterable<Tuple2<IntValue, Integer>> iterable, Collector<Tuple2<IntValue, Integer>> collector) throws Exception {
+            IntValue rating = null;
+            int ratingsCount = 0;
+            for (Tuple2<IntValue, Integer> tuple : iterable) {
+                rating = tuple.f0;
+                ratingsCount += tuple.f1;
+            }
+
+            collector.collect(new Tuple2<>(rating, ratingsCount));
+        }
+    }
+}
